bcd13c4d2b75176b15f080fc70a1487e2f13549b
Rahat Ahmed Chowdhury <61385277+RahatGithub@users.noreply.github.com>
2026-06-10 03:12:10 +0600

    fix: suppress hydration warning from browser extension attribute injection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${sora.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster position="top-right" richColors />
